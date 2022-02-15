cc2e0813f205686982281973328f4fc1a3a3267e
Ben <benparker03@gmail.com>
2022-02-15 13:29:12 +0000

    separated application and server into separate modules with server now in bin/www

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 
 const routes = require('./routes');
-const { port } = require('./config');
 
 const app = express();
 
 app.use(routes);
 
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+module.exports = app;
