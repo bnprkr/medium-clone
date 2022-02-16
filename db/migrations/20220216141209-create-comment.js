1e1c5e954e2ac024c941f90e2ea1561a064f4822
Ben <benparker03@gmail.com>
2022-02-16 14:14:43 +0000

    generated comment model and migration; edited migration not allow null values and link foreign keys to story and user tables

---
+++ db/migrations/20220216141209-create-comment.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      storyId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'Stories' }
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'Users' }
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      commentText: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Comments');
+  }
+};
