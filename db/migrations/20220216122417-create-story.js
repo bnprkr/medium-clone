dc333ef37efad1a67dc9a1951e01a674b9d5a004
Ben <benparker03@gmail.com>
2022-02-16 13:22:33 +0000

    story model and migration created, migration edited

---
+++ db/migrations/20220216122417-create-story.js
@@ -0,0 +1,37 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Stories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'Users' }
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      storyText: {
+        allowNull: false,
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Stories');
+  }
+};
