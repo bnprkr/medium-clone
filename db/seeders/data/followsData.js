4495847d59a783235174822c42e711f805d96c1d
Ben <benparker03@gmail.com>
2022-02-17 14:36:38 +0000

    fixed follows data seeding so user cannot follow themselves

--- db/seeders/data/followsData.js
+++ db/seeders/data/followsData.js
@@ -14,8 +14,9 @@ const follows = [];
 for (let i = 1; i <= numUsers; i++) { 
   const numFollowing = Math.floor(Math.random() * (maxFollowing - minFollowing + 1)) + minFollowing;
   const users = userIds.slice()
+  users.splice(i - 1, 1);
   for (let j = 0; j < numFollowing; j++) {
-    const randIndex = Math.floor(Math.random() * (numUsers - j));
+    const randIndex = Math.floor(Math.random() * (numUsers - j - 1));
     const sampledId = users.splice(randIndex, 1)[0];
     follows.push(
       {
