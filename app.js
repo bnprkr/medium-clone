7bd9dfce8cc9e22a1ca5633cec393ca02194034b
Ben <benparker03@gmail.com>
2022-02-18 14:16:32 +0000

    updated app to use session, loading secret from environment variable

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const morgan = require('morgan');
+const session = require('express-session');
 
+const { secret } = require('../config').session;
 const authRoutes = require('./routes/auth');
 
 const app = express();
 
 app.set('view engine', 'pug');
-
+app.use(session({
+  secret,
+  resave: false,
+  saveUninitialised: false
+}));
 app.use(morgan('dev'));
 app.use(authRoutes);
 
