9a3d029cb2db3cef690ce85523f474595dd8b2a4
Ben <benparker03@gmail.com>
2022-02-20 15:47:34 +0000

    basic outline for request handlers for register get and post routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,41 @@
-// authentication related routes 
-// /signup
-// /login
-
 const express = require('express');
+const { check, validationResult } = require('express-validator');
+
+const db = require('../db/models');
+const { csrfProtection, asyncHandler } = require('./utils'); 
 
 const router = express.Router();
 
-router.get('/signup', (req, res) => {
-  res.send('OK');
+router.get('/register', csrfProtection, (req, res) => {
+  // const user = db.User.build();
+  res.render('register', {
+    title: 'Register',
+    // user, 
+    csrfToken: req.csrfToken(),
+  });
+
+  const userValidators = [
+    // TODO define validators
+  ];
+});
+
+router.post('/register', csrfProtection, (req, res) => {
+  asyncHandler(async (req, res) => {
+    const {
+      username,
+      emailAddress,
+      password,
+    } = req.body;
+
+    const user = db.User.build({
+      username,
+      email,
+    });
+
+    const validatorErrors = validationResult(req);
+
+    
+  })
 });
 
 router.get('/login', (req, res) => {
