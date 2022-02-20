286166144ec8cf3c4238d7793b872dc9673808ba
Ben <benparker03@gmail.com>
2022-02-20 18:11:51 +0000

    added validations for username, password, confirmPassword

--- routes/auth.js
+++ routes/auth.js
@@ -19,7 +19,7 @@ const userValidators = [
     .exists({ checkFalsy: true })
     .withMessage('Please provide a value for Email Address')
     .isLength({ max: 255 })
-    .withMessage('Email Address length cannot be longer than 255 characters')
+    .withMessage('Email Address cannot be longer than 255 characters')
     .isEmail()
     .withMessage('Email Address is not a valid email')
     .custom(value => {
@@ -29,7 +29,38 @@ const userValidators = [
             return Promise.reject('Email already in use.');
           }
         });
-    })
+    }),
+    check('username')
+      .exists({ checkFalsy: true })
+      .withMessage('Please enter a Username')
+      .isLength({ max: 50 })
+      .withMessage('Username cannot not be more than 50 characters long')
+      .custom(value => {
+        return db.User.findOne({ where: { username: value } })
+          .then(user => {
+            if (user) {
+              return Promise.reject('Username already in use.');
+            }
+          });
+      }),
+      check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for Password')
+        .isLength({ max: 50 })
+        .withMessage('Password must not be more than 50 characters long')
+        .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, 'g')
+        .withMessage('Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'),
+      check('confirmPassword')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a value for Confirm Password')
+        .isLength({ max: 50 })
+        .withMessage('Confirm Password must not be more than 50 characters long')
+        .custom((value, { req }) => {
+          if (value !== req.body.password) {
+            throw new Error('Confirm Password does not match Password');
+          }
+          return true;
+        }),
 ];
 
 router.post('/register', csrfProtection, userValidators, (req, res) => {
