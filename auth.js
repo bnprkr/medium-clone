b4d5d1381fd9b8970c51c52b0be384ae28e601b8
Ben <benparker03@gmail.com>
2022-02-22 13:34:33 +0000

    added restoreUser middleware function to auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,5 @@
+const { db } = require("./config");
+
 const loginUser = (req, res, user) => {
   req.session.auth = {
     userId: user.id,
@@ -5,6 +7,32 @@ const loginUser = (req, res, user) => {
   };
 };  
 
+const restoreUser = async (req, res, next) => {
+  console.log(req.session);
+
+  if (req.session.auth) {
+    const { userId } = req.session.auth;
+
+    try {
+      const user = await db.User.findByPk(userId);
+
+      if (user) {
+        res.locals.authenticated = true;
+        res.locals.user = user;
+        next();
+      }
+    } catch (err) {
+      res.locals.authenticated = false;
+      next(err);
+    }
+  } else {
+    res.locals.authenticated = false;
+    next();
+  }
+};
+
+
 module.exports = {
   loginUser,
+  restoreUser
 }
