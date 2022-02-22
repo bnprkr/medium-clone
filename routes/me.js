401d4e9b6f44b48757956e70438a41c3bcfb7c6b
Ben <benparker03@gmail.com>
2022-02-22 11:37:58 +0000

    renamed routes files for clarity

---
+++ routes/me.js
@@ -0,0 +1,4 @@
+// routes specific to currently logged in user
+// / homepage with story feed
+// /me/stories 
+// /me/follow
--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,96 @@
-// routes specific to currently logged in user
-// / homepage with story feed
-// /me/stories 
-// /me/follow
+const express = require('express');
+const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+const db = require('../db/models');
+const { csrfProtection, asyncHandler } = require('./utils'); 
+const { userValidators, loginValidators } = require('./validators')
+
+const router = express.Router();
+
+router.get('/register', csrfProtection, (req, res) => {
+  res.render('register', {
+    title: 'Register',
+    user: {}, 
+    csrfToken: req.csrfToken(),
+  });
+});
+
+router.post('/register', csrfProtection, userValidators,
+  asyncHandler(async (req, res) => {
+    const {
+      username,
+      email,
+      password,
+    } = req.body;
+
+    const user = db.User.build({
+      username,
+      email,
+    });
+
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user.hashedPassword = hashedPassword;
+      await user.save();
+      res.redirect('/');
+    } else {
+      const errors = validatorErrors.array().map(errorObj => errorObj.msg);
+      res.render('register', {
+        title: 'Register',
+        user, 
+        errors, 
+        csrfToken: req.csrfToken()
+      });
+    }
+
+  })
+);
+
+router.get('/login', csrfProtection, (req, res) => {
+  res.render('login', {
+    title: 'Login', 
+    csrfToken: req.csrfToken(),
+  });
+});
+
+router.post('/login', csrfProtection, loginValidators, 
+  asyncHandler(async (req, res) => {
+    const {
+      email, 
+      password
+    } = req.body;
+
+    let errors = [];
+    const validatorErrors = validationResult(req);
+
+    if (validatorErrors.isEmpty()) {
+      const user = await db.User.findOne({ where: { email }});
+
+      if (user !== null) {
+        const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
+
+        if (passwordMatch) {
+          // TODO login user
+          return res.redirect('/');
+        }
+      }
+
+      errors.push('Login failed for the provided email address and password.');
+    
+    } else {
+      errors = validatorErrors.array().map(errorObj => errorObj.msg);
+    }
+
+    res.render('login', {
+      title: 'Login', 
+      email, 
+      errors, 
+      csrfToken: req.csrfToken(),
+    });
+
+  }));
+
+module.exports = router;
