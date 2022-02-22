a31214c8e4c4721b1f675e047039199f03d43d2d
Ben <benparker03@gmail.com>
2022-02-22 16:42:42 +0000

    removed username from loginUser as not needed there

--- auth.js
+++ auth.js
@@ -2,8 +2,7 @@ const db = require("./db/models");
 
 const loginUser = (req, res, user) => {
   req.session.auth = {
-    userId: user.id,
-    username: user.username,
+    userId: user.id
   };
 };  
 
