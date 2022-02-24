22f4a2150ca611d0d69e3e9d0f868bb629089969
Ben <benparker03@gmail.com>
2022-02-24 12:33:11 +0000

    updated app.js to serve static files from /public

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 const session = require('express-session');
 const store = require('connect-pg-simple');
 const cookieParser = require('cookie-parser');
+const path = require("path");
 
 const { secret } = require('./config').session;
 const authRoutes = require('./routes/user');
@@ -14,6 +15,7 @@ const { restoreUser } = require('./auth');
 const app = express();
 
 app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cookieParser(secret));
 app.use(session({
   // note secure cookie needed in production with expiry time...
