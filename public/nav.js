02e72f7071ced59e2ec418480565d069639c1c63
Ben <benparker03@gmail.com>
2022-05-26 10:45:08 +0100

    added javascript to listen to toggle button for mobile menu

---
+++ public/nav.js
@@ -0,0 +1,22 @@
+const menu = document.querySelector(".menu");
+const toggle = document.querySelector(".toggle");
+
+// toggle menu for mobile view
+function toggleMenu() {
+
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+
+    // add menu (hamburger) icon
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    menu.classList.add("active");
+
+    // add menu close (x) icon
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+  }
+}
+
+// add event listener to toggle
+toggle.addEventListener('click', toggleMenu);
+
