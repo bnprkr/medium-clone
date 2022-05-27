e94ae240bdccd9585335c63af5be73130eae3059
Ben <benparker03@gmail.com>
2022-05-27 14:57:18 +0100

    tidying story.js

--- public/story.js
+++ public/story.js
@@ -1,26 +1,17 @@
 const likeButton = document.querySelector("i.like-button");
 
 likeButton.addEventListener("click", async (event) => {
-  console.log(event.target.classList);
 
   const storyId = window.location.href.split('/')[5];
-  console.log(window.location.href);
-  console.log(storyId);
 
   const likes = document.querySelector(".story-likes > span");
 
   try {
-    // const res = await fetch(`http://localhost:8080/api/stories/${storyId}/like`, {
-    //   method: 'POST',
-    // });
-
     const res = await fetch(`http://localhost:8080/api/stories/${storyId}/like`, {
       method: 'GET',
     });
 
     const { liked } = await res.json();
-
-    console.log(liked);
 
     if (liked) {
       // if liked delete like
