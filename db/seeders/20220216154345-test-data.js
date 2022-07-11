a58ff28ae243a1b01c584cba68da013c29835d12
Ben <benparker03@gmail.com>
2022-07-11 16:19:44 +0100

    removed seeding of commentLikes to reduce database size

--- db/seeders/20220216154345-test-data.js
+++ db/seeders/20220216154345-test-data.js
@@ -1,9 +1,16 @@
-'use strict';
+"use strict";
 
-const { users, stories, comments, storyLikes, commentLikes, follows } = require('./data');
+const {
+  users,
+  stories,
+  comments,
+  storyLikes,
+  // commentLikes,
+  follows,
+} = require("./data");
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     /*
      * Add seed commands here.
      *
@@ -12,30 +19,29 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
 
-    await queryInterface.bulkInsert('Users', users, {});  
-    await queryInterface.bulkInsert('Stories', stories, {});
-    await queryInterface.bulkInsert('Comments', comments, {});
-    await queryInterface.bulkInsert('StoryLikes', storyLikes, {});
-    await queryInterface.bulkInsert('CommentLikes', commentLikes, {});
-    return queryInterface.bulkInsert('Follows', follows, {});
-
+    await queryInterface.bulkInsert("Users", users, {});
+    await queryInterface.bulkInsert("Stories", stories, {});
+    await queryInterface.bulkInsert("Comments", comments, {});
+    await queryInterface.bulkInsert("StoryLikes", storyLikes, {});
+    // await queryInterface.bulkInsert('CommentLikes', commentLikes, {});
+    return queryInterface.bulkInsert("Follows", follows, {});
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     /*
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
-    */
+     */
 
-    await queryInterface.bulkDelete('CommentLikes', null, {});
-    await queryInterface.bulkDelete('StoryLikes', null, {});
-    await queryInterface.bulkDelete('Comments', null, {});
-    await queryInterface.bulkDelete('Stories', null, {});
-    await queryInterface.bulkDelete('Follows', null, {});
-    return queryInterface.bulkDelete('Users', null, {});
-  }
+    // await queryInterface.bulkDelete('CommentLikes', null, {});
+    await queryInterface.bulkDelete("StoryLikes", null, {});
+    await queryInterface.bulkDelete("Comments", null, {});
+    await queryInterface.bulkDelete("Stories", null, {});
+    await queryInterface.bulkDelete("Follows", null, {});
+    return queryInterface.bulkDelete("Users", null, {});
+  },
 };
--- db/seeders/data/likesData.js
+++ db/seeders/data/likesData.js
@@ -1,35 +1,31 @@
-const { numStories, stories } = require('./storiesData');
-const { numComments, comments } = require('./commentsData');
+const { numStories, stories } = require("./storiesData");
+// const { numComments, comments } = require("./commentsData");
 
 const numStoryLikes = numStories * 55;
-const numCommentLikes = numComments * 12;
+// const numCommentLikes = numComments * 12;
 
 const storyLikes = [];
 
 for (let i = 0; i < numStoryLikes; i++) {
   const storyIndex = Math.floor(Math.random() * numStories);
-  storyLikes.push(
-    {
-      userId: 2,
-      storyId: storyIndex + 1,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }
-  );
+  storyLikes.push({
+    userId: 2,
+    storyId: storyIndex + 1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  });
 }
 
-const commentLikes = [];
+// const commentLikes = [];
 
-for (let i = 0; i < numCommentLikes; i++) {
-  const commentIndex = Math.floor(Math.random() * numComments);
-  commentLikes.push(
-    {
-      userId: comments[commentIndex].userId,
-      commentId: commentIndex + 1,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }
-  );
-}
+// for (let i = 0; i < numCommentLikes; i++) {
+//   const commentIndex = Math.floor(Math.random() * numComments);
+//   commentLikes.push({
+//     userId: comments[commentIndex].userId,
+//     commentId: commentIndex + 1,
+//     createdAt: new Date(),
+//     updatedAt: new Date(),
+//   });
+// }
 
-module.exports = { storyLikes, commentLikes }
+module.exports = { storyLikes };
